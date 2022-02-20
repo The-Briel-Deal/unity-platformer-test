bdf4a05a5089730e73cdf79ac79f8eb78a27568e
The-Briel-Deal <gabe@fordltc.net>
2022-02-19 21:33:03 -0500

    Got movement working

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform camTF = this.GetComponentInParent<Transform>();
+        print(player.transform.position);
+        camTF.position = player.transform.position;
+
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float horiInput = 0f;
+    public bool jump = false;
     public CharacterController2D controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        jump = Input.GetButton("Jump");
+        horiInput = Input.GetAxis("Horizontal");
+        Debug.Log(horiInput.ToString());
+        controller.Move(horiInput, false, jump);
     }
 }
+
